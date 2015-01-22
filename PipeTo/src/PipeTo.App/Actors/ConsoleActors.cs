657e8642839e970dd411c79b13261b394833d879
Aaron Stannard <aaron@petabridge.com>
2015-01-22 15:36:01 -0800

    initial actor types and definitions

--- PipeTo/src/PipeTo.App/Actors/ConsoleActors.cs
+++ PipeTo/src/PipeTo.App/Actors/ConsoleActors.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Akka.Actor;
-using Akka.Util.Internal;
 
 namespace PipeTo.App.Actors
 {
@@ -9,13 +8,61 @@ namespace PipeTo.App.Actors
     /// </summary>
     public class ConsoleWriterActor : UntypedActor
     {
+        #region ConsoleWriterActor message types
+
+        /*
+         * You don't have to define message types for actors inside that actor's class definition.
+         * Any C# type is a valid message type.
+         * 
+         * However, for messages that are received only by one specific type of actor, this convention
+         * of using nested class types is helpful as it makes the intended recipient of the message
+         * obvious.
+         */
+
+        /// <summary>
+        /// Message class used to send colored console messages
+        /// </summary>
+        public class ConsoleWriteMsg
+        {
+            public ConsoleWriteMsg(string message) : this(message, ConsoleColor.Gray)
+            {
+            }
+
+            public ConsoleWriteMsg(string message, ConsoleColor color)
+            {
+                Color = color;
+                Message = message;
+            }
+
+            public ConsoleColor Color { get; private set; }
+            public string Message { get; private set; }
+        }
+
+        #endregion
+
         protected override void OnReceive(object message)
         {
+            /*
+             * This actor can receieve either a ConsoleWriteMsg object or a string,
+             * it will conditionally check for either upon each received message and
+             * mark any other types of messages as 'Unhandled' in the event that an
+             * unsupported type is sent to this actor.
+             */
+
+            var consoleWriteMsg = message as ConsoleWriteMsg;
+            if (consoleWriteMsg != null)
+            {
+                Console.ForegroundColor = consoleWriteMsg.Color;
+                Console.WriteLine("{0}: {1}", Sender, consoleWriteMsg.Message);
+                return;
+            }
+
             var str = message as string; //cast the message back into a string
             if (string.IsNullOrEmpty(str)) //invalid message
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("ERROR! Received a non-string object! How did you manage that?!");
+                Console.WriteLine("{0}: ERROR! Received a non-string object! How did you manage that?!", Sender);
+                Unhandled(message);
                 return;
             }
 
@@ -29,6 +76,8 @@ namespace PipeTo.App.Actors
     /// </summary>
     public class ConsoleReaderActor : UntypedActor
     {
+
+        #region ConsoleReaderActor Message classes
         /// <summary>
         /// Message used to signal that we just need to read from the console
         /// </summary>
@@ -48,10 +97,20 @@ namespace PipeTo.App.Actors
             public string PreviousUrl { get; private set; }
         }
 
+        #endregion
+
         public const string EscapeString = "exit";
 
         protected override void OnReceive(object message)
         {
+            //If the message is neither of our supported message types
+            if (!(message is ReadFromConsoleError || message is ReadFromConsoleClean))
+            {
+                //let's the system know this message was unhandled. Gets published to log.
+                Unhandled(message);
+                return;
+            }
+
             if (message is ReadFromConsoleError)
                 Instructions.PrintRssParseError(message as ReadFromConsoleError);
             Instructions.PrintRssInstructions();
---
+++ PipeTo/src/PipeTo.App/Actors/FeedValidatorActor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using QDFeedParser;
+
+namespace PipeTo.App.Actors
+{
+    /// <summary>
+    /// This actor's job is to validate the following:
+    ///  1. That the provided string is, in fact, a URL
+    ///  2. That the URL in question is, in fact, an ATOM / RSS feed
+    /// </summary>
+    public class FeedValidatorActor : ReceiveActor
+    {
+
+        #region FeedValidatorActor message types
+
+        /// <summary>
+        /// Message class used to pipe the result of whether or not a feed is valid.
+        /// </summary>
+        public class IsValidFeed
+        {
+            public IsValidFeed(Uri feedUri, bool isValid)
+            {
+                IsValid = isValid;
+                FeedUri = feedUri;
+            }
+
+            public bool IsValid { get; private set; }
+
+            public Uri FeedUri { get; private set; }
+        }
+
+        #endregion
+
+        private readonly IFeedFactory _feedFactory;
+
+        public FeedValidatorActor(IFeedFactory feedFactory)
+        {
+            _feedFactory = feedFactory;
+
+            //We're using a ReceiveActor, so we can define different receive functions for individual types of messages.
+            Receive<string>(s =>
+            {
+
+                if (IsValidUrl(s))
+                {
+                    
+                }
+            });
+        }
+
+        #region Feed validation methods
+
+        public bool IsValidUrl(string strToTest)
+        {
+            return Uri.IsWellFormedUriString(strToTest, UriKind.Absolute);
+        }
+
+        public async Task<bool> IsValidRssOrAtomFeed(Uri rssUrl)
+        {
+            return await _feedFactory.PingFeedAsync(rssUrl);
+        }
+
+        #endregion
+
+        #region Console write methods
+
+        public bool SendMessage(string message, bool isSuccess = true)
+        {
+            var consoleColor = isSuccess ? ConsoleColor.Green : ConsoleColor.DarkRed;
+            var consoleMsg = new ConsoleWriterActor.ConsoleWriteMsg(message, consoleColor);
+            Context.ActorSelection(ActorNames.ConsoleWriterActor.Path).Tell(consoleMsg);
+        }
+
+        #endregion
+    }
+}
