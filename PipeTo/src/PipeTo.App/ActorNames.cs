523c79b44b5b5313d94c8ef0aedbff6811c20d94
Andrew Skotzko <andrew.skotzko@gmail.com>
2015-03-07 22:06:57 -0800

    update actor paths to work on mono

--- PipeTo/src/PipeTo.App/ActorNames.cs
+++ PipeTo/src/PipeTo.App/ActorNames.cs
@@ -12,17 +12,17 @@ namespace PipeTo.App
         /// <summary>
         /// Responsible for serializing writes to the <see cref="Console"/>
         /// </summary>
-        public static readonly ActorData ConsoleWriterActor = new ActorData("consoleWriter", "/user"); // /user/consoleWriter
+		public static readonly ActorData ConsoleWriterActor = new ActorData("consoleWriter", "akka://MyFirstActorSystem/user"); // /user/consoleWriter
 
         /// <summary>
         /// Responsible for serializing reads from the <see cref="Console"/>
         /// </summary>
-        public static readonly ActorData ConsoleReaderActor = new ActorData("consoleReaderActor", "/user"); // /user/consoleReader
+		public static readonly ActorData ConsoleReaderActor = new ActorData("consoleReaderActor", "akka://MyFirstActorSystem/user"); // /user/consoleReader
 
         /// <summary>
         /// Responsible for validating data from <see cref="ConsoleReaderActor"/> and kicking off the feed parsing process
         /// </summary>
-        public static readonly ActorData FeedValidatorActor = new ActorData("feedValidator", "/user"); // /user/feedValidator
+		public static readonly ActorData FeedValidatorActor = new ActorData("feedValidator", "akka://MyFirstActorSystem/user"); // /user/feedValidator
     }
 
     /// <summary>
@@ -38,7 +38,7 @@ namespace PipeTo.App
 
         public ActorData(string name)
         {
-            Path = "/" + name;
+			Path = "akka://MyFirstActorSystem/" + name;
             Name = name;
         }
 
