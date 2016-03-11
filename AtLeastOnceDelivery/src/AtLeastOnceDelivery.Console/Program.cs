3724276fec7e43e16d054e759b95e7f8e5b08868
Aaron Stannard <aaron@petabridge.com>
2016-03-11 13:24:20 -0800

    added AtLeastOnceDeliverySample

---
+++ AtLeastOnceDelivery/src/AtLeastOnceDelivery.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AtLeastOnceDelivery.Console
+{
+    using Akka.Actor;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var actorSystem = ActorSystem.Create("AtLeastOnceDeliveryDemo"))
+            {
+                var recipientActor = actorSystem.ActorOf(Props.Create(() => new MyRecipientActor()), "receiver");
+                var atLeastOnceDeliveryActor =
+                    actorSystem.ActorOf(Props.Create(() => new MyAtLeastOnceDeliveryActor(recipientActor)), "delivery");
+
+                actorSystem.WhenTerminated.Wait();
+            }
+        }
+    }
+}
