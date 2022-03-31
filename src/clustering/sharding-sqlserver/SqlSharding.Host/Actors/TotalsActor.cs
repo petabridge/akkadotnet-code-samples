8e9629835a3090e4129942dd96d0aeddd503704f
Aaron Stannard <aaron@petabridge.com>
2022-03-31 12:01:42 -0500

    Initial Sharding Sample

---
+++ src/clustering/sharding-sqlserver/SqlSharding.Host/Actors/TotalsActor.cs
@@ -0,0 +1,18 @@
+﻿using Akka.Persistence;
+
+namespace SqlSharding.Host.Actors;
+
+public sealed class TotalsActor : ReceivePersistentActor
+{
+    /// <summary>
+    /// Used to help differentiate what type of entity this is inside Akka.Persistence's database
+    /// </summary>
+    public const string TotalsEntityNameConstant = "totals";
+    
+    public TotalsActor(string persistenceId)
+    {
+        PersistenceId = $"{TotalsEntityNameConstant}-" + persistenceId;
+    }
+
+    public override string PersistenceId { get; }
+}
