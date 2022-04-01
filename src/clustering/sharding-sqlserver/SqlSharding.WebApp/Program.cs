ae263bb3ccfe67677912171467166786e1ea3f39
Aaron Stannard <aaron@petabridge.com>
2022-03-31 19:55:03 -0500

    added Akka.NET to Web App

--- src/clustering/sharding-sqlserver/SqlSharding.WebApp/Program.cs
+++ src/clustering/sharding-sqlserver/SqlSharding.WebApp/Program.cs
@@ -1,7 +1,38 @@
+using Akka.Actor;
+using Akka.Cluster.Hosting;
+using Akka.Hosting;
+using Akka.Remote.Hosting;
+using SqlSharding.Shared.Sharding;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+
+var akkaSection = builder.Configuration.GetSection("Akka");
+
+// maps to environment variable Akka__ClusterIp
+var hostName = akkaSection.GetValue<string>("ClusterIp", "localhost");
+
+// maps to environment variable Akka__ClusterPort
+var port = akkaSection.GetValue<int>("ClusterPort", 7918);
+
+var seeds = akkaSection.GetValue<string[]>("ClusterSeeds", new []{ "akka.tcp://SqlSharding@localhost:7919" }).Select(Address.Parse)
+    .ToArray();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddAkka("SqlSharding", (configurationBuilder, provider) =>
+{
+    configurationBuilder.WithRemoting(hostName, port)
+        .WithClustering(new ClusterOptions()
+            { Roles = new[] { "Web" }, SeedNodes = seeds })
+        .WithShardRegionProxy<ProductMarker>("products", ProductActorProps.SingletonActorRole,
+            new ProductMessageRouter())
+        .WithActors((system, registry) =>
+        {
+            var proxyProps = system.ProductIndexProxyProps();
+            registry.TryRegister<ProductIndexMarker>(system.ActorOf(proxyProps, "product-proxy"));
+        });
+});
 
 var app = builder.Build();
 
