52562d248d4a4a9508f6edbc0ceb25ca262af064
Aaron Stannard <aaron@petabridge.com>
2022-04-01 08:00:52 -0500

    added first simple CRUD page

--- src/clustering/sharding-sqlserver/SqlSharding.WebApp/Pages/CreateProduct.cshtml.cs
+++ src/clustering/sharding-sqlserver/SqlSharding.WebApp/Pages/CreateProduct.cshtml.cs
@@ -1,11 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel.DataAnnotations;
+using Akka.Actor;
+using Akka.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SqlSharding.Shared.Commands;
+using SqlSharding.Shared.Sharding;
 
 namespace SqlSharding.WebApp.Pages;
 
+
+public sealed class NewProduct
+{
+    [BindProperty]
+    [Required]
+    [MinLength(6)]
+    [MaxLength(50)]
+    public string ProductName { get; set; }
+    
+    [Range(1, 100000)]
+    [DataType(DataType.Currency)]
+    public decimal Price { get; set; }
+    
+    [Range(0, 100000)]
+    public int InitialQuantity { get; set; }
+}
+
 public class CreateProduct : PageModel
 {
+    public CreateProduct(ActorRegistry registry)
+    {
+        _productActor = registry.Get<ProductMarker>();
+    }
+
+    [BindProperty]
+    public NewProduct Product { get; set; }
+    
+    private readonly IActorRef _productActor;
+    
     public void OnGet()
     {
         
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var createProductCommand = new SqlSharding.Shared.Commands.CreateProduct(Guid.NewGuid().ToString(),
+            Product.ProductName, Product.Price, Product.InitialQuantity);
+
+        var createRsp = await _productActor.Ask<ProductCommandResponse>(createProductCommand, TimeSpan.FromSeconds(3));
+
+        return RedirectToPage("./product", new { productId = createProductCommand.ProductId });
     }
 }
