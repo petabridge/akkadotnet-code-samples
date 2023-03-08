89c9aa8e8a26c45425003631080ec92ba9d29934
Aaron Stannard <aaron@petabridge.com>
2023-03-08 15:21:38 -0600

    move state processing to extension methods (#122)

--- src/clustering/sharding-sqlserver/SqlSharding.Shared/ProductState.cs
+++ src/clustering/sharding-sqlserver/SqlSharding.Shared/ProductState.cs
@@ -14,6 +14,151 @@ public record PurchasingTotals(int RemainingInventory, int SoldInventory, decima
     public static readonly PurchasingTotals Empty = new(0, 0, decimal.Zero);
 }
 
+public static class ProductStateExtensions
+{
+    /// <summary>
+    /// Stateful processing of commands. Performs input validation et al.
+    /// </summary>
+    /// <remarks>
+    /// Intentionally kept simple.
+    /// </remarks>
+    /// <param name="productState">The product state we're evaluating.</param>
+    /// <param name="productCommand">The command to process.</param>
+    /// <returns></returns>
+    public static ProductCommandResponse ProcessCommand(this ProductState productState, IProductCommand productCommand)
+    {
+        switch (productCommand)
+        {
+            case CreateProduct create when productState.IsEmpty:
+            {
+                if (productState.IsEmpty)
+                {
+                    // not initialized, can create
+
+                    // events
+                    var productCreated = new ProductCreated(create.ProductId, create.ProductName, create.Price);
+                    var productInventoryChanged = new ProductInventoryChanged(create.ProductId, create.InitialQuantity,
+                        DateTime.UtcNow,
+                        InventoryChangeReason.SupplyIncrease);
+
+
+                    var response = new ProductCommandResponse(create.ProductId,
+                        new IProductEvent[] { productCreated, productInventoryChanged });
+                    return response;
+                }
+                else
+                {
+                    return new ProductCommandResponse(productState.Data.ProductId, Array.Empty<IProductEvent>(), false,
+                        $"Product with [Id={productState.Data.ProductId}] already exists");
+                }
+            }
+            case SupplyProduct(var productId, var additionalQuantity) when !productState.IsEmpty:
+            {
+                var productInventoryChanged = new ProductInventoryChanged(productId, additionalQuantity, DateTime.UtcNow,
+                    InventoryChangeReason.SupplyIncrease);
+                var response = new ProductCommandResponse(productId,
+                    new IProductEvent[] { productInventoryChanged });
+                return response;
+            }
+            // if Price is 0, then it is likely not set
+            case PurchaseProduct purchase when !productState.IsEmpty && productState.Data.CurrentPrice > 0:
+            {
+                var events = new List<IProductEvent>();
+                var productInventoryChanged = new ProductInventoryChanged(purchase.ProductId, -1*purchase.NewOrder.Quantity,
+                    DateTime.UtcNow,
+                    InventoryChangeReason.Fulfillment);
+                events.Add(productInventoryChanged);
+                var backordered = false;
+
+                if (productState.Totals.RemainingInventory - purchase.NewOrder.Quantity <= ProductState.LowInventoryWarningThreshold)
+                {
+                    if (productState.Totals.RemainingInventory <= 0)
+                    {
+                        backordered = true;
+                        var warningEvent = new ProductInventoryWarningEvent(purchase.ProductId,
+                            ProductWarningReason.NoSupply, DateTime.UtcNow, $"Product [Id={productState.Data.ProductId}, Name={productState.Data.ProductName}] is now on backorder. [{productState.Totals.RemainingInventory}] available inventory.");
+                        events.Add(warningEvent);
+                    }
+                    else
+                    {
+                        var warningEvent = new ProductInventoryWarningEvent(purchase.ProductId,
+                            ProductWarningReason.LowSupply, DateTime.UtcNow, $"Product [Id={productState.Data.ProductId}, Name={productState.Data.ProductName}] is low on supply. [{productState.Totals.RemainingInventory}] available inventory.");
+                        events.Add(warningEvent);
+                    }
+                }
+
+                var productSold = new ProductSold(purchase.NewOrder, productState.Data.CurrentPrice, backordered);
+                events.Add(productSold);
+
+                return new ProductCommandResponse(purchase.ProductId, events);
+            }
+            default:
+            {
+                return new ProductCommandResponse(productCommand.ProductId, Array.Empty<IProductEvent>(), false,
+                    $"Product with [Id={productState.Data.ProductId}] is not ready to process command [{productCommand}]");
+            }
+        }
+    }
+
+    public static ProductState ProcessEvent(this ProductState productState, IProductEvent productEvent)
+    {
+        /*
+         *     var newTotals = new PurchasingTotals(create.InitialQuantity, 0, decimal.Zero);
+                    var newData = new ProductData(create.ProductId, create.ProductName, create.Price);
+                    var newProduct = this with { Data = newData, Totals = newTotals };
+         * 
+         */
+        switch (productEvent)
+        {
+            case ProductCreated(var productId, var productName, var price):
+            {
+                return productState with
+                {
+                    Data = productState.Data with
+                    {
+                        ProductId = productId, CurrentPrice = price,
+                        ProductName = productName
+                    }
+                };
+            }
+            case ProductInventoryChanged(var productId, var quantity, var timestamp, var inventoryChangeReason) @event:
+            {
+                return productState with
+                {
+                    Totals = productState.Totals with
+                    {
+                        RemainingInventory = productState.Totals.RemainingInventory + quantity,
+                        SoldInventory = inventoryChangeReason == InventoryChangeReason.Fulfillment
+                            ? productState.Totals.SoldInventory + Math.Abs(quantity)
+                            : productState.Totals.SoldInventory
+                    },
+                    InventoryChanges = productState.InventoryChanges.Add(@event)
+                };
+            }
+            case ProductInventoryWarningEvent warning:
+            {
+                return productState with
+                {
+                    Warnings = productState.Warnings.Add(warning)
+                };
+            }
+            case ProductSold sold:
+            {
+                return productState with
+                {
+                    Orders = productState.Orders.Add(sold),
+                    Totals = productState.Totals with
+                    {
+                        TotalRevenue = productState.Totals.TotalRevenue + sold.TotalPrice
+                    }
+                };
+            }
+            default:
+                throw new ArgumentOutOfRangeException(nameof(productEvent));
+        }
+    }
+}
+
 /// <summary>
 /// The state object responsible for all event and message processing
 /// </summary>
@@ -35,146 +180,4 @@ public record ProductState : IWithProductId
     public ImmutableSortedSet<ProductInventoryChanged> InventoryChanges { get; init; } = ImmutableSortedSet<ProductInventoryChanged>.Empty;
 
     public bool IsEmpty => Data == ProductData.Empty;
-
-
-    /// <summary>
-    /// Stateful processing of commands. Performs input validation et al.
-    /// </summary>
-    /// <remarks>
-    /// Intentionally kept simple.
-    /// </remarks>
-    /// <param name="productCommand">The command to process.</param>
-    /// <returns></returns>
-    public ProductCommandResponse ProcessCommand(IProductCommand productCommand)
-    {
-        switch (productCommand)
-        {
-            case CreateProduct create when IsEmpty:
-            {
-                if (IsEmpty)
-                {
-                    // not initialized, can create
-
-                    // events
-                    var productCreated = new ProductCreated(create.ProductId, create.ProductName, create.Price);
-                    var productInventoryChanged = new ProductInventoryChanged(create.ProductId, create.InitialQuantity,
-                        DateTime.UtcNow,
-                        InventoryChangeReason.SupplyIncrease);
-
-
-                    var response = new ProductCommandResponse(create.ProductId,
-                        new IProductEvent[] { productCreated, productInventoryChanged });
-                    return response;
-                }
-                else
-                {
-                    return new ProductCommandResponse(Data.ProductId, Array.Empty<IProductEvent>(), false,
-                        $"Product with [Id={Data.ProductId}] already exists");
-                }
-            }
-            case SupplyProduct(var productId, var additionalQuantity) when !IsEmpty:
-            {
-                var productInventoryChanged = new ProductInventoryChanged(productId, additionalQuantity, DateTime.UtcNow,
-                    InventoryChangeReason.SupplyIncrease);
-                var response = new ProductCommandResponse(productId,
-                    new IProductEvent[] { productInventoryChanged });
-                return response;
-            }
-            // if Price is 0, then it is likely not set
-            case PurchaseProduct purchase when !IsEmpty && Data.CurrentPrice > 0:
-            {
-                var events = new List<IProductEvent>();
-                var productInventoryChanged = new ProductInventoryChanged(purchase.ProductId, -1*purchase.NewOrder.Quantity,
-                    DateTime.UtcNow,
-                    InventoryChangeReason.Fulfillment);
-                events.Add(productInventoryChanged);
-                var backordered = false;
-
-                if (Totals.RemainingInventory - purchase.NewOrder.Quantity <= LowInventoryWarningThreshold)
-                {
-                    if (Totals.RemainingInventory <= 0)
-                    {
-                        backordered = true;
-                        var warningEvent = new ProductInventoryWarningEvent(purchase.ProductId,
-                            ProductWarningReason.NoSupply, DateTime.UtcNow, $"Product [Id={Data.ProductId}, Name={Data.ProductName}] is now on backorder. [{Totals.RemainingInventory}] available inventory.");
-                        events.Add(warningEvent);
-                    }
-                    else
-                    {
-                        var warningEvent = new ProductInventoryWarningEvent(purchase.ProductId,
-                            ProductWarningReason.LowSupply, DateTime.UtcNow, $"Product [Id={Data.ProductId}, Name={Data.ProductName}] is low on supply. [{Totals.RemainingInventory}] available inventory.");
-                        events.Add(warningEvent);
-                    }
-                }
-
-                var productSold = new ProductSold(purchase.NewOrder, Data.CurrentPrice, backordered);
-                events.Add(productSold);
-
-                return new ProductCommandResponse(purchase.ProductId, events);
-            }
-            default:
-            {
-                return new ProductCommandResponse(productCommand.ProductId, Array.Empty<IProductEvent>(), false,
-                    $"Product with [Id={Data.ProductId}] is not ready to process command [{productCommand}]");
-            }
-        }
-    }
-
-    public ProductState ProcessEvent(IProductEvent productEvent)
-    {
-        /*
-         *     var newTotals = new PurchasingTotals(create.InitialQuantity, 0, decimal.Zero);
-                    var newData = new ProductData(create.ProductId, create.ProductName, create.Price);
-                    var newProduct = this with { Data = newData, Totals = newTotals };
-         * 
-         */
-        switch (productEvent)
-        {
-            case ProductCreated(var productId, var productName, var price):
-            {
-                return this with
-                {
-                    Data = Data with
-                    {
-                        ProductId = productId, CurrentPrice = price,
-                        ProductName = productName
-                    }
-                };
-            }
-            case ProductInventoryChanged(var productId, var quantity, var timestamp, var inventoryChangeReason) @event:
-            {
-                return this with
-                {
-                    Totals = Totals with
-                    {
-                        RemainingInventory = Totals.RemainingInventory + quantity,
-                        SoldInventory = inventoryChangeReason == InventoryChangeReason.Fulfillment
-                            ? Totals.SoldInventory + Math.Abs(quantity)
-                            : Totals.SoldInventory
-                    },
-                    InventoryChanges = InventoryChanges.Add(@event)
-                };
-            }
-            case ProductInventoryWarningEvent warning:
-            {
-                return this with
-                {
-                    Warnings = Warnings.Add(warning)
-                };
-            }
-            case ProductSold sold:
-            {
-                return this with
-                {
-                    Orders = Orders.Add(sold),
-                    Totals = Totals with
-                    {
-                        TotalRevenue = Totals.TotalRevenue + sold.TotalPrice
-                    }
-                };
-            }
-            default:
-                throw new ArgumentOutOfRangeException(nameof(productEvent));
-        }
-    }
 }
