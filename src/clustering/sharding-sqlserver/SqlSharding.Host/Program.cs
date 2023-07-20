0cf690bf0430b41120736deed6419fcbd5c4354b
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-07-20 10:26:08 +0700

    Make the SQL server sharding sample completely hoconless

--- src/clustering/sharding-sqlserver/SqlSharding.Host/Program.cs
+++ src/clustering/sharding-sqlserver/SqlSharding.Host/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Akka.Actor;
+using Akka.Cluster;
 using Akka.Cluster.Hosting;
 using Akka.Cluster.Sharding;
+using Akka.Event;
 using Akka.Hosting;
 using Akka.Persistence.Hosting;
 using Akka.Persistence.SqlServer.Hosting;
@@ -19,6 +21,8 @@ using SqlSharding.Shared.Serialization;
 using SqlSharding.Shared.Sharding;
 
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+var seedDb = Environment.GetEnvironmentVariable("SEED_DB")?.ToLowerInvariant() is "true" ||
+             (args.Length > 0 && args[0] == "seed-db");
 
 var builder = new HostBuilder()
     .ConfigureAppConfiguration(c => c.AddEnvironmentVariables()
@@ -28,7 +32,8 @@ var builder = new HostBuilder()
     {
         // maps to environment variable ConnectionStrings__AkkaSqlConnection
         var connectionString = context.Configuration.GetConnectionString("AkkaSqlConnection");
-
+        if (connectionString is null)
+            throw new Exception("AkkaSqlConnection setting is missing");
 
         var akkaSection = context.Configuration.GetSection("Akka");
 
@@ -39,45 +44,100 @@ var builder = new HostBuilder()
         var port = akkaSection.GetValue<int>("ClusterPort", 0);
 
         var seeds = akkaSection.GetValue<string[]>("ClusterSeeds", new[] { "akka.tcp://SqlSharding@localhost:7918" })
-
             .ToArray();
 
         services.AddAkka("SqlSharding", (configurationBuilder, provider) =>
         {
+            #region Custom sharding journal options setup
+
+            var shardingJournalOptions = new SqlServerJournalOptions(
+                isDefaultPlugin: false, 
+                identifier: "shading")
+            {
+                ConnectionString = connectionString,
+                TableName = "ShardingEventJournal", 
+                MetadataTableName = "ShardingMetadata",
+                AutoInitialize = true
+            };
+            shardingJournalOptions.Adapters.AddWriteEventAdapter<MessageTagger>("tagger", new[] { typeof(object) });
+
+            #endregion
+
+            #region Custom sharding snapshot store options setup
+
+            var shardingSnapshotOptions = new SqlServerSnapshotOptions(
+                isDefaultPlugin: false, 
+                identifier: "sharding")
+            {
+                ConnectionString = connectionString,
+                TableName = "ShardingSnapshotStore",
+                AutoInitialize = true
+            };
+
+            #endregion
+            
             configurationBuilder
                 .WithRemoting(hostName, port)
                 .AddAppSerialization()
-                .WithClustering(new ClusterOptions()
-                    { Roles = new[] { ProductActorProps.SingletonActorRole }, SeedNodes = seeds })
+                .WithClustering(new ClusterOptions
+                {
+                    Roles = new[] { ProductActorProps.SingletonActorRole }, 
+                    SeedNodes = seeds
+                })
                 .WithSqlServerPersistence(connectionString, journalBuilder: builder =>
                 {
                     builder.AddWriteEventAdapter<MessageTagger>("product-tagger", new[] { typeof(IProductEvent) });
                 })
-                .WithShardRegion<ProductMarker>("products",
-                    s => ProductTotalsActor.GetProps(s), new ProductMessageRouter(),
-                    new ShardOptions()
+                .WithJournalAndSnapshot(shardingJournalOptions, shardingSnapshotOptions)
+                .WithShardRegion<ProductMarker>(
+                    typeName: "products",
+                    entityPropsFactory: ProductTotalsActor.GetProps, 
+                    messageExtractor: new ProductMessageRouter(),
+                    shardOptions: new ShardOptions
                     {
-                        Role = ProductActorProps.SingletonActorRole, RememberEntities = true,
+                        Role = ProductActorProps.SingletonActorRole, 
+                        RememberEntities = true,
                         StateStoreMode = StateStoreMode.DData,
                         RememberEntitiesStore = RememberEntitiesStore.Eventsourced,
-                        JournalPluginId = "akka.persistence.journal.sharding",
-                        SnapshotPluginId = "akka.persistence.snapshot-store.sharding"
+                        JournalOptions = shardingJournalOptions,
+                        SnapshotOptions = shardingSnapshotOptions, 
+                        FailOnInvalidEntityStateTransition = true
                     })
                 .WithClusterShardingJournalMigrationAdapter("akka.persistence.journal.sharding")
-                .AddHoconFile("sharding.conf", HoconAddMode.Prepend)
-                .AddHocon(@$"akka.persistence.journal.sharding.connection-string = ""{connectionString}""
-                akka.persistence.snapshot-store.sharding.connection-string = ""{connectionString}""
-                ", HoconAddMode.Prepend)
-                .WithSingleton<ProductIndexActor>("product-proxy",
-                    (_, _, resolver) => resolver.Props<ProductIndexActor>(),
-                    new ClusterSingletonOptions() { Role = ProductActorProps.SingletonActorRole })
-                // .WithSingleton<ProductCreatorActor>("product-creator",
-                //     (system, registry, resolver) => resolver.Props<ProductCreatorActor>(21_000, 100),
-                //     new ClusterSingletonOptions() { Role = ProductActorProps.SingletonActorRole })
+                .WithSingleton<ProductIndexActor>(
+                    singletonName: "product-proxy",
+                    propsFactory: (_, _, resolver) => resolver.Props<ProductIndexActor>(),
+                    options: new ClusterSingletonOptions
+                    {
+                        Role = ProductActorProps.SingletonActorRole
+                    })
                 .AddPetabridgeCmd(cmd =>
                 {
                     cmd.RegisterCommandPalette(ClusterShardingCommands.Instance);
                     cmd.RegisterCommandPalette(ClusterCommands.Instance);
+                })
+                .AddStartup((system, registry) =>
+                {
+                    if (!seedDb) 
+                        return;
+                    
+                    var log = Logging.GetLogger(system, nameof(Program));
+                    var actor = registry.Get<ProductMarker>();
+                    var generator = new FakeDataGenerator(actor);
+                    
+                    var cluster = Cluster.Get(system);
+                    cluster.RegisterOnMemberUp(() =>
+                    {
+                        generator.Generate(100, log)
+                            .ContinueWith(t =>
+                            {
+                                if (!t.IsCompletedSuccessfully)
+                                {
+                                    log.Error(t.Exception, "Failed to generate fake data");
+                                }
+                            })
+                            .ConfigureAwait(false);
+                    });
                 });
         });
     })
---
+++ src/clustering/sharding-sqlserver/SqlSharding.Shared/FakeDataGenerator.cs
@@ -0,0 +1,50 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using Bogus;
+using SqlSharding.Shared.Commands;
+
+namespace SqlSharding.Shared;
+
+public class FakeDataGenerator
+{
+    private readonly IActorRef _shardRegion;
+    private readonly Faker<CreateProduct> _productGenerator;
+
+    public FakeDataGenerator(IActorRef shardRegion)
+    {
+        _shardRegion = shardRegion;
+        _productGenerator = new Faker<CreateProduct>()
+            .UseSeed(123456)
+            //.StrictMode(true)
+            .CustomInstantiator(f =>
+            {
+                var count = f.Random.Int(0, 3);
+                return new CreateProduct(
+                    f.Random.Guid().ToString(),
+                    f.Commerce.ProductName(),
+                    f.Finance.Amount(min: 1, max: 100000),
+                    f.Random.Int(1, 100000),
+                    count == 0 ? Array.Empty<string>() : f.Commerce.Categories(count)
+                );
+            });
+    }
+
+    public async Task Generate(int count, ILoggingAdapter log)
+    {
+        log.Info($"Generating {count} fake data");
+        var total = 0;
+        foreach (var _ in Enumerable.Range(0, count))
+        {
+            var response = await _shardRegion.Ask<ProductCommandResponse>(_productGenerator.Generate(), TimeSpan.FromSeconds(3));
+            if (!response.Success)
+            {
+                log.Error(response.Message);
+            }
+            else
+            {
+                total++;
+            }
+        }
+        log.Info($"Generated {total} fake data");
+    }
+}
