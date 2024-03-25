a54d491e449480713d0ae47dad77e69286ede5a7
Aaron Stannard <aaron@petabridge.com>
2024-03-25 14:28:14 -0500

    added EF context

---
+++ src/cqrs/cqrs-sqlserver/CqrsSqlServer.DataModel/Migrations/20240325192428_InitialCreat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CqrsSqlServer.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    RemainingInventory = table.Column<int>(type: "int", nullable: false),
+                    SoldUnits = table.Column<int>(type: "int", nullable: false),
+                    TotalRevenue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
