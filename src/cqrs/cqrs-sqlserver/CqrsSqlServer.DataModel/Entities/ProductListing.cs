0dea7df50f12ca7de4d5310ac8930337ce7634bd
Aaron Stannard <aaron@petabridge.com>
2024-03-25 14:12:13 -0500

    defining EF contexts

---
+++ src/cqrs/cqrs-sqlserver/CqrsSqlServer.DataModel/Entities/ProductListing.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ProductListing.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2024 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace CqrsSqlServer.DataModel.Entities;
+
+public class ProductListing
+{
+    public string ProductId { get; set; } = null!;
+    
+    public string ProductName { get; set; } = null!;
+    
+    public decimal Price { get; set; }
+    
+    public int RemainingInventory { get; set; }
+    
+    public int SoldUnits { get; set; }
+    
+    public decimal TotalRevenue { get; set; }
+    
+    public DateTime Created { get; set; }
+    
+    public DateTime LastModified { get; set; }
+}
